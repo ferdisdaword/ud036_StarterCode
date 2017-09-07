a23fd502fa9c907d28f3cc9d6190f3dcc0582c72
Unknown <fferrao3@gmail.com>
2017-09-06 18:22:45 -0700

    Ptoject: Movie Trailer Website
    Udacity Full Stack Web Developer Nanodegree Program

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -1,7 +1,9 @@
 import webbrowser
 import os
 import re
-
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 # Styles and scripting for the page
 main_page_head = '''
@@ -19,6 +21,9 @@ main_page_head = '''
     <style type="text/css" media="screen">
         body {
             padding-top: 80px;
+        }
+        .black {
+            color: black;
         }
         #trailer .modal-dialog {
             margin-top: 200px;
@@ -104,10 +109,10 @@ main_page_content = '''
 
     <!-- Main Page Content -->
     <div class="container">
-      <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="navbar navbar-fixed-top" role="navigation" style="background-color: #00FFCC;">
         <div class="container">
           <div class="navbar-header">
-            <a class="navbar-brand" href="#">Fresh Tomatoes Movie Trailers</a>
+            <a class="navbar-brand" href="#"><span class="black">Fresh Tomatoes Movie Trailers</span></a>
           </div>
         </div>
       </div>
@@ -123,8 +128,10 @@ main_page_content = '''
 # A single movie entry html template
 movie_tile_content = '''
 <div class="col-md-6 col-lg-4 movie-tile text-center" data-trailer-youtube-id="{trailer_youtube_id}" data-toggle="modal" data-target="#trailer">
-    <img src="{poster_image_url}" width="220" height="342">
-    <h2>{movie_title}</h2>
+    <h4>{movie_title}</h4>
+    <img src="https://image.tmdb.org/t/p/w640/{poster_image_url}" width="220" height="342">
+    <h6 align="justify">{movie_storyline}</h6>
+    <p><strong>Rating: {movie_rating}</strong></p>
 </div>
 '''
 
@@ -134,19 +141,21 @@ def create_movie_tiles_content(movies):
     content = ''
     for movie in movies:
         # Extract the youtube ID from the url
-        youtube_id_match = re.search(
-            r'(?<=v=)[^&#]+', movie.trailer_youtube_url)
-        youtube_id_match = youtube_id_match or re.search(
-            r'(?<=be/)[^&#]+', movie.trailer_youtube_url)
-        trailer_youtube_id = (youtube_id_match.group(0) if youtube_id_match
-                              else None)
-
+        youtube_id_match = [item['key'] for item in movie.results]
+        youtube_id_match = youtube_id_match[0]
+        trailer_youtube_id = youtube_id_match
+        # Pull the US rating for each movie
+        for c in movie.countries:
+            if c['iso_3166_1'] == 'US':
+                rating = c['certification']
         # Append the tile for the movie with its content filled in
         content += movie_tile_content.format(
             movie_title=movie.title,
-            poster_image_url=movie.poster_image_url,
-            trailer_youtube_id=trailer_youtube_id
-        )
+            movie_storyline=movie.overview,
+            poster_image_url=movie.poster_path,
+            trailer_youtube_id=trailer_youtube_id,
+            movie_rating=rating)
+
     return content
 
 
